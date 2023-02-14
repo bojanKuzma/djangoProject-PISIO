af760711f616abf325be8051f525bfeb2073e128
Bojan Kuzmanovic <bojan.kuzmanovic@student.etf.unibl.org>
2023-02-14 14:40:44 +0100

    7.5. Changed urls.py

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from forum.views import Home, Base
+from forum.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Base.as_view(), name='base'),
+    path('', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
     path('home', Home.as_view(), name='home'),
 ]
